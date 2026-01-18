de8145548ea01dd46f921b1b23df8c2485b9f764
koeunchan54 <koeunchan54@gmail.com>
2026-01-18 18:35:34 +0900

    app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Text Length Counter with Buttons")
 
 text = st.text_area("Enter text", height=200)
 
-if st.button("Count):
+if st.button("Count"):
   st.write(f"Character Count: {len(text)}")
