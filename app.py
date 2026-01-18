9c70d81a495669ff019de6eea87c3da76dec66ea
koeunchan54 <koeunchan54@gmail.com>
2026-01-18 18:35:13 +0900

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("Text Length Counter")
+st.title("Text Length Counter with Buttons")
 
 text = st.text_area("Enter text", height=200)
 
-if text:
+if st.button("Count):
   st.write(f"Character Count: {len(text)}")
