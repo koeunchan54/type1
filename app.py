d6beba190edab113968a1b7555f08e8cfb5503b7
koeunchan54 <koeunchan54@gmail.com>
2026-01-18 18:24:39 +0900

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
-st.write("My first web app... strange")
+st.title("Text Length Counter")
+
+text = st.text_area("Enter text", height=200)
+
+if text:
+  st.wirte(f"Character Count: {len(text)}")
